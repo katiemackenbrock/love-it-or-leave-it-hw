39c687ed0d0cee6f3d54f1e9edc548e95c2924f9
katiemackenbrock <katiemackenbrock@gmail.com>
2021-01-11 11:04:38 -0800

    oops fixed my error

--- controllers/leaveIt.js
+++ controllers/leaveIt.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/movies', (req, res) => {
-    res.render('leave-it/movies', {movies: ['any musical', 'anything on hallmark', 'why cant every movie be as funny as my cousin vinny tbh]})
+    res.render('leave-it/movies', {movies: ['any musical', 'anything on hallmark', 'why cant every movie be as funny as my cousin vinny tbh']})
 });
 
 router.get('/products', (req, res) => {
